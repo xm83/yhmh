db2e63026c16936d0ceb6154f902a8b79657c807
violakyoungalee <kyounga.lee@yale.edu>
2018-08-01 14:07:23 -0700

    frontend modifications

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -1,17 +1,27 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Homepage extends Component {
-  constructor(props){
-    super(props);
-    this.state = {}
-  }
+const HomePage = () => (
+  <div>
+    <h1>Home Page</h1>
+    <Link to="/login">Login</Link>
+  </div>
+);
 
-  render() {
-    return (<div>
-      <Button variant="contained" onClick={this.onCreate}>Create</Button><br/>
-      <TextField id="docId" label="Join Doc" value={this.state.docId} onChange={this.onChange('docId')} margin="normal"/>
-      <Button variant="contained" onClick={this.onJoin}>Join</Button><br/>
-      <Button variant="contained" onClick={this.onLogout}>Logout</Button><br/>
-    </div>)
-  }
-}
+export default HomePage;
+//
+// export default class Homepage extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {}
+//   }
+//
+//   render() {
+//     return (<div>
+//       <Button variant="contained" onClick={this.onCreate}>Create</Button><br/>
+//       <TextField id="docId" label="Join Doc" value={this.state.docId} onChange={this.onChange('docId')} margin="normal"/>
+//       <Button variant="contained" onClick={this.onJoin}>Join</Button><br/>
+//       <Button variant="contained" onClick={this.onLogout}>Logout</Button><br/>
+//     </div>)
+//   }
+// }
